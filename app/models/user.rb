ded1534c47b59c11cd847ab7847418e9b49d4df5
Jovita <jovitaspais@gmail.com>
2022-02-07 20:59:06 +0000

    add sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   # helper validates that the attribute's value is unique right before the object gets saved
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :email, presence: true, :uniqueness => {:case_sensitive => true}
+  validates :password, presence: true, confirmation: true, length: {within: 6..40}
+  validates :password_confirmation, presence: true, length: {within: 6..40}
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before :each do @user = User.new(first_name: 'Barbara', last_name: 'Brown', email: 'barbara@brown.com', password: 'BarbaraBrown', password_confirmation: 'BarbaraBrown')
+  end
+  
+  describe 'Validations' do
+
+    it 'is valid with valid attributes' do
+      expect(@user).to be_valid
+    end
+
+    it "is not valid without a first name" do
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a last name" do
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a email" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with an email that has already been used" do
+      @user.save
+      user1 = User.new(first_name: 'Barbara', last_name: 'Brown', email: 'barbara@brown.com', password: 'BarbaraBrown', password_confirmation: 'BarbaraBrown')
+      expect(user1).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with a short password" do
+      @user.password = 'barbara'
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a password confirmation" do
+      @user.password_confirmation = nil
+      expect(@user).not_to be_valid
+    end
+
+  end
+end
